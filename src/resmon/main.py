9b29d554b2a302609f16a8e7c51e0d0e7272aaca
Christopher Wetherill <chris@tbmh.org>
2021-07-15 14:57:08 -0400

    Return ok/notokay status on request
    This adds a `/v1/resources` GET endpoint with optional `cpu`
    and `ram` URL parameters. The endpoint will return an 'OK' status
    for any nodes with capacity equal to or greater than that
    specified in the request, or 'NOTOKAY' for any nodes with less
    than that capacity on either criterion. I still need to do a
    bit of testing to make sure that if I add/remove nodes to/from
    the cluster while this service is running it'll pick them up
    correctly, and need to test with heterogeneously provisioned
    nodes, but then should be good to stub out a quick manifest
    and get this deployed into the cluster.

--- src/resmon/main.py
+++ src/resmon/main.py
@@ -1,9 +1,8 @@
-import os
 import threading
 import time
-import json
+import copy
 from kubernetes import client, config
-from flask import Flask
+from flask import Flask, request
 
 
 NODES = {}
@@ -18,7 +17,7 @@ def update_node_list(k8s_client=v1):
         node_dict = {}
         for node in nodes.items:
             node_dict[str(node.metadata.name)] = {
-                'cpu': node.status.capacity['cpu'],
+                'cpu': float(node.status.capacity['cpu']),
                 'ram': int(node.status.capacity['memory'].strip('Ki'))
             }
 
@@ -33,9 +32,22 @@ t.start()
 t.setName('nodeUpdater')
 
 
-@app.route("/")
-def hello_world():
-    return NODES
+@app.route('/v1/resources', methods=['GET'])
+@app.route('/v1/resources/', methods=['GET'])
+def check_node_resources():
+    _cpu = request.args.get('cpu', 0., type=float)
+
+    # URL parameter is in MB, value returned from K8s API is in KB
+    _ram = request.args.get('ram', 0, type=int) * 1000
+
+    node_status = copy.deepcopy(NODES)
+    for node_name, node_val in node_status.items():
+        if (node_val['cpu'] >= _cpu) and (node_val['ram'] >= _ram):
+            node_status[node_name]['status'] = 'OK'
+        else:
+            node_status[node_name]['status'] = 'NOTOKAY'
+
+    return node_status
 
 
 if __name__ == '__main__':
